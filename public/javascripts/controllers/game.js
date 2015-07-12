a4095996e8b5fe0e2973ef109c08b0bed04f134d
Kevin Hinds <khinds@buydomains.com>
2015-07-12 13:39:03 -0400

    game page with map!

--- public/javascripts/controllers/game.js
+++ public/javascripts/controllers/game.js
@@ -3,8 +3,26 @@
  * 
  * @author khinds (c) shamanic.io, http://www.shamanic.io
  */
-var gameController = angular.module("gameController", []);
+var gameController = angular.module("gameController", ['sigilService']);
 
-gameController.controller("gameController", [ '$scope', '$http', function($scope, $http) {
-	console.log('game js here');
+gameController.controller("gameController", [ '$scope', 'sigilService', function($scope, sigilService) {
+	
+	//$scope.mapData = {};
+	$scope.sigilList = [];	
+	
+	$scope.imageList = [
+		{
+			url: 'http://placekitten.com/201/201',
+			name: 'sigilOne'
+		},
+		{
+			url: 'http://placekitten.com/201/203',
+			name: 'sigilTwo'
+		}
+	];
+	sigilService.success(function(data) {
+	  console.log('controller succeeded in getting map data');
+	  $scope.mapData = data;
+	  console.log('mapData from controller: ', $scope.mapData);
+	});
 } ]);
