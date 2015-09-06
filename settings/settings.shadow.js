f60a290e2ec88b528f953f52a55737c44e0c499f
Kevin Hinds <khinds10.work@gmail.com>
2015-09-05 22:47:29 -0400

    update settings shadow file message

--- settings/settings.shadow.js
+++ settings/settings.shadow.js
@@ -3,7 +3,7 @@
  * 		(please create your own personal 'settings.js' file with your own values from the template below)
  * 
  * 		WARNING: 
- * 				Never commit your own 'settings.js' to GIT, it's a public repo and you'll give away your passwords :) 
+ * 				Never check in your own 'settings.js' to GIT, if it's a public repo and you'll give away your passwords :) 
  */
 
 /** postgres DB */
