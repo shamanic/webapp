f8bcd87be6df7a3855f6aabb813c3445be8eb7ff
davidps <davidpschmitz@gmail.com>
2020-02-22 17:27:00 -0600

    changed db to use env vars

--- db/config.js
+++ db/config.js
@@ -3,5 +3,29 @@
  */
 var AppSettings = require('../settings/settings.js');
 var DBWrapper = require('node-dbi').DBWrapper;
-var dbConnectionConfig = { host:AppSettings.DBConfig.db.host, user:AppSettings.DBConfig.db.username, password:AppSettings.DBConfig.db.password, database:AppSettings.DBConfig.db.database };
-exports.dbWrapper = new DBWrapper('pg', dbConnectionConfig);
+var dbConnectionConfig = { host:AppSettings.DBConfig.db.host, 
+    user:AppSettings.DBConfig.db.username, 
+    password:AppSettings.DBConfig.db.password, 
+    database:AppSettings.DBConfig.db.database 
+};
+var regex = /(.*):\/\/([a-z].*):([0-9a-f].*)@(.*):([0-9]{4})\/([a-z0-9].*)/g;
+var groups = process.env.DATABASE_URL.match(regex);
+/*  groups[0]=dbAdapterName(postgres)
+*   ://
+*   groups[1]=username
+*   :
+*   groups[2]=password
+*   @
+*   groups[3]=host
+*   :
+*   groups[4]=port
+*   /
+*   groups[5]=database
+*/
+var herokuDbConnCfg = {
+    host: groups[3],
+    user: groups[1],
+    password: groups[2],
+    database: groups[5]
+}
+exports.dbWrapper = new DBWrapper('pg', herokuDbConnCfg);
